70f6889fc1f76fec946bac1b06af93fa40d5392a
Edin Grech <edin.grech@gmail.com>
2024-01-03 01:00:52 +0100

    removed testing data

--- DataAccess/Structures/StructHelpers.cs
+++ DataAccess/Structures/StructHelpers.cs
@@ -13,14 +13,14 @@ namespace DataAccess.Structures
 
         public class BoardData
         {
-            public int id { get; set; }
+            public int? id { get; set; } = null;
             public int PlayerID;
             public string PlayerUsername { get; set; }
             public int GameID;
             public List<Attack> Attacks { get; set; }
             public List<ShipInfo> Ships { get; set; }
 
-            public int hitPoints { get; set; }
+            public int hitPoints { get; set; } = 0;
 
             public BoardData(int playerID, string playerUsername, int gameID)
             {
@@ -28,56 +28,8 @@ namespace DataAccess.Structures
                 PlayerUsername = playerUsername;
                 GameID = gameID;
 
-                //temp prefill
-                Attacks = new List<Attack>
-                {
-                    new Attack{
-                            hit = true,
-                            coordinate = new Coordinate { Row = "A", Col = "1" } 
-                        },
-                    new Attack{
-                            hit = true,
-                            coordinate = new Coordinate { Row = "A", Col = "2" }
-                        },
-                    new Attack{
-                            hit = true,
-                            coordinate = new Coordinate { Row = "A", Col = "3" }
-                        },
-                    new Attack{
-                            hit = false,
-                            coordinate = new Coordinate { Row = "A", Col = "4" }
-                        }
-                };
-                Ships = new List<ShipInfo>
-                {
-                    new ShipInfo
-                    {
-                        OrientedCoordinate = new OrientedCoordinate
-                        {
-                            Coordinate = new Coordinate { Row = "D", Col = "4" },
-                            Orientation = Orientation.Horizontal
-                        },
-                        Size = 2
-                    },
-                    new ShipInfo
-                    {
-                        OrientedCoordinate = new OrientedCoordinate
-                        {
-                            Coordinate = new Coordinate { Row = "E", Col = "5" },
-                            Orientation = Orientation.Vertical
-                        },
-                        Size = 3
-                    },
-                    new ShipInfo
-                    {
-                        OrientedCoordinate = new OrientedCoordinate
-                        {
-                            Coordinate = new Coordinate { Row = "F", Col = "6" },
-                            Orientation = Orientation.Horizontal
-                        },
-                        Size = 3
-                    }
-                };
+                Attacks = new List<Attack>();
+                Ships = new List<ShipInfo>();
             }
         }
     }
