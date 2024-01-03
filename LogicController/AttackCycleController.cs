567d5078db41593d3e804e6fef3781da7b1259f6
Edin Grech <edin.grech@gmail.com>
2024-01-03 01:07:37 +0100

    feat: Attack state management

--- LogicController/AttackCycleController.cs
+++ LogicController/AttackCycleController.cs
@@ -5,21 +5,22 @@ using static FrontEnd.FrontEndController;
 
 namespace LogicController
 {
-    internal static class AttackCycleController
+    internal class AttackCycleController
     {
-        
-        public static Tuple<
+        Grid grid1 = new();
+        Grid grid2 = new();
+        bool gameEnd = false;
+
+        public Tuple<
             BoardData,
             BoardData,
             bool> AttackRound(Player player1, Player player2, BoardData playerBoard1, BoardData playerBoard2)
         {
-            Grid grid = new();
-            bool gameEnd = false;
             DataAccess.Structures.StructHelpers.Attack attack1;
             DataAccess.Structures.StructHelpers.Attack attack2;
 
-            (playerBoard2, attack1) = AttackShip(playerBoard2, player1, grid);
-            (playerBoard1, attack2) = AttackShip(playerBoard1, player2, grid);
+            (playerBoard2, attack1) = AttackShip(playerBoard2, player1, grid1);
+            (playerBoard1, attack2) = AttackShip(playerBoard1, player2, grid2);
 
             SaveTranslationController.Attack2BoardSaver(attack1, playerBoard2);
             SaveTranslationController.Attack2BoardSaver(attack2, playerBoard1);
@@ -27,11 +28,13 @@ namespace LogicController
             if (playerBoard1.hitPoints <= 0)
             {
                 Console.WriteLine($"Player {player2.username} Has won!");
+                Console.ReadKey();
                 gameEnd = true;
             }
             if (playerBoard2.hitPoints <= 0)
             {
                 Console.WriteLine($"Player {player1.username} Has won!");
+                Console.ReadKey();
                 gameEnd = true;
             }
 
